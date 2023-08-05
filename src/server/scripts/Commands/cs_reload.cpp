1ea07368b7efdcff747eba961d88cd3f045fe3d6
cristian-root <delta_root@hotmail.com>
2023-08-04 19:48:51 -0500

    Fix command reload npc

--- src/server/scripts/Commands/cs_reload.cpp
+++ src/server/scripts/Commands/cs_reload.cpp
@@ -450,7 +450,7 @@ public:
     {
         if (args.empty())
             { 
-			LOG_INFO("Re-Loading Creature templates...");
+			LOG_INFO("server.loading","Re-Loading Creature templates...");
             sObjectMgr->LoadCreatureTemplates();
             handler->SendGlobalGMSysMessage("DB table `Creature_template` reloaded.");
             return true;
